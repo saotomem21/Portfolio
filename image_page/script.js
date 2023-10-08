673d80428bfb254385cf2a2fa72b0632c2851c5f
saotomem21 <87118475+saotomem21@users.noreply.github.com>
2023-10-08 21:48:57 +0900

    Update script.js

--- image_page/script.js
+++ image_page/script.js
@@ -1,5 +1,28 @@
 function displayImage(event) {
-    var uploadedImage = document.getElementById('uploadedImage');
-    uploadedImage.src = URL.createObjectURL(event.target.files[0]);
-    uploadedImage.style.display = 'block';
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+    var img = new Image();
+    img.src = URL.createObjectURL(event.target.files[0]);
+    img.onload = function() {
+        var displayWidth = img.width;
+        var displayHeight = img.height;
+        canvas.style.display = 'block';
+        canvas.width = displayWidth;
+        canvas.height = displayHeight;
+        ctx.drawImage(img, 0, 0, displayWidth, displayHeight);
+    }
+}
+
+function makeBlack() {
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+    var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    var data = imageData.data;
+    for (var i = 0; i < data.length; i += 4) {
+        var grayscale = data[i] * 0.3 + data[i + 1] * 0.59 + data[i + 2] * 0.11;
+        data[i] = grayscale;     // 赤
+        data[i + 1] = grayscale; // 緑
+        data[i + 2] = grayscale; // 青
+    }
+    ctx.putImageData(imageData, 0, 0);
 }
