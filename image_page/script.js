4d07c3820f9ed8d8ce5bea395732ee7f833e1d74
saotomem21 <87118475+saotomem21@users.noreply.github.com>
2023-10-08 21:56:27 +0900

    Update script.js

--- image_page/script.js
+++ image_page/script.js
@@ -1,3 +1,5 @@
+var originalImageData = null;
+
 function displayImage(event) {
     var canvas = document.getElementById('canvas');
     var ctx = canvas.getContext('2d');
@@ -10,6 +12,7 @@ function displayImage(event) {
         canvas.width = displayWidth;
         canvas.height = displayHeight;
         ctx.drawImage(img, 0, 0, displayWidth, displayHeight);
+        originalImageData = ctx.getImageData(0, 0, canvas.width, canvas.height);  // 画像データを保存
     }
 }
 
@@ -20,9 +23,17 @@ function makeBlack() {
     var data = imageData.data;
     for (var i = 0; i < data.length; i += 4) {
         var grayscale = data[i] * 0.3 + data[i + 1] * 0.59 + data[i + 2] * 0.11;
-        data[i] = grayscale;     // 赤
-        data[i + 1] = grayscale; // 緑
-        data[i + 2] = grayscale; // 青
+        data[i] = grayscale;
+        data[i + 1] = grayscale;
+        data[i + 2] = grayscale;
     }
     ctx.putImageData(imageData, 0, 0);
+}
+
+function restoreImage() {
+    if (originalImageData) {
+        var canvas = document.getElementById('canvas');
+        var ctx = canvas.getContext('2d');
+        ctx.putImageData(originalImageData, 0, 0);  // 元の画像データを復元
+    }
 }
